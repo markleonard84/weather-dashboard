3a392250a51d6c4856ee17c4c2aa51807ae0ac5a
Mark Leonard <marketh@me.com>
2020-10-22 10:58:13 +0100

    updated to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,16 @@ $(document).ready(function() {
         const locationData = $("#userLocationInput").val();
         console.log('location Data is', locationData);
 
-        const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + locationData + "&appid=7e6dc8803f9cdeac4c39d2ea0c3c4c03";
+        const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + locationData + "&units=metric&appid=7e6dc8803f9cdeac4c39d2ea0c3c4c03";
         console.log('queryURL is', queryURL);
 
         $.ajax({
             url: queryURL
         }).then(function(weatherData) {
-            console.log('weatherData is',
-                weatherData);
+            console.log('weatherData is', weatherData);
+            console.log('name of searched city', weatherData.name);
+            console.log('current temperature', weatherData.main.temp);
+            console.log('icon is?', weatherData.weather[0].icon);
         })
 
     });
