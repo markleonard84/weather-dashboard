66468a642a08de635cfe55f00411797d3d856785
Mark Leonard <marketh@me.com>
2020-10-23 22:27:41 +0100

    updates to Index.html and script.js. Data call from API, display on page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,39 @@
-$(document).ready(function() {
+const recentSearchList = $("#recentHistory");
+const currentConditions = $("#currentConditions");
+const fiveDayForcast = $("#fiveDayForcast");
+const locationData = $("#searchLocation input[name='locationInput']")
+const locationSearchForm = $("#searchLocation");
 
+locationSearchForm.submit(function(event) {
+    event.preventDefault();
+    const requestedLocation = locationData.val();
+    const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + requestedLocation + "&units=metric&appid=7e6dc8803f9cdeac4c39d2ea0c3c4c03";
 
-    const submitBtn = $("#locationSubmitBtn");
-    const recentSearchList = $("#recentViewList");
-    const currentConditions = $("#currentConditions");
-    const fiveDayForcast = $("#fiveDayForcast");
+    $.ajax({
+        url: queryURL
+    }).then(function(weatherData) {
+        console.log('weatherData', weatherData);
 
+        createDailyForcast(weatherData);
 
-    // Gets the location user has submitted and the calls weather data.
-    $(submitBtn).click(function(event) {
-        event.preventDefault();
-        const locationData = $("#userLocationInput").val();
-        console.log('location Data is', locationData);
-
-        const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + locationData + "&units=metric&appid=7e6dc8803f9cdeac4c39d2ea0c3c4c03";
-        console.log('queryURL is', queryURL);
-
-        $.ajax({
-            url: queryURL
-        }).then(function(weatherData) {
-            console.log('weatherData is', weatherData);
-            console.log('name of searched city', weatherData.name);
-            console.log('current temperature', weatherData.main.temp);
-            console.log('icon is?', weatherData.weather[0].icon);
-        })
+    }).catch(function(err) {
 
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
+
+
+function createDailyForcast(weatherData) {
+    let forcastAtLocation = $("<div>").attr('class= "card"');
+    let locationName = $("<h6>").addClass("card-title").text(weatherData.name);
+    let currentTemperature = $("<p>").text("Current Temperature: " + weatherData.main.temp);
+    let tempFeelsLike = $("<p>").text("Feels like: " + weatherData.main.feels_like)
+    let currentDiscription = $("<p>").text("Current Conditions: " + weatherData.weather[0].description);
+    let forcastImage = $("<img>").attr({
+        src: "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png",
+        height: "auto",
+        width: "100px"
+    });
+    currentConditions.append(forcastAtLocation, locationName, currentTemperature, tempFeelsLike, currentDiscription, forcastImage);
+
+    console.log('image', weatherData.weather[0].icon);
+};
